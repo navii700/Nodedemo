830160580bc0c7abb8a4dad51dc49e933099c79f
navii700 <65499906+navii700@users.noreply.github.com>
2023-12-30 16:17:07 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var router = express.Router();
-const {dashboard , register ,login ,userregister ,loginuser ,logout} = require('../controller/userController')
+const {dashboard , register ,login ,userregister ,loginuser ,logout , profile ,saveprofile} = require('../controller/userController')
 const checkAuthUser = require('../middleware/validateToken');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login');
+  res.redirect('/login');
 });
 router.get('/forgot-password', function(req, res, next) {
   res.render('forgot-password');
 });
 
 router.route('/dashboard').get(checkAuthUser, dashboard);
-router.get('/register',register);
+router.route('/profile').get(checkAuthUser, profile).post(saveprofile);
+router.route('/register').get(register).post(userregister);
+router.route('/login').get(login).post(loginuser);
+
 router.get('/logout',logout);
-router.get('/login',login);
-router.post('/register',userregister);
-router.post('/login',loginuser);
 
 module.exports = router;
